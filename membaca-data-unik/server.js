ac80af6c9058b1b1800a14209a420f746f00f508
eka prasasti <ekaputraprasasti@gmail.com>
2016-10-26 08:02:46 +0700

    add another project

---
+++ membaca-data-unik/server.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mobil');
+
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('connection success');
+});
+
+var mobilSchema = mongoose.Schema({
+	nama: String,
+	merk: String,
+	tahunPembuatan: Number,
+	tanggalUpdate: Date,
+	transmisi: {
+		manual: Boolean,
+		outomatic: Boolean
+	}
+});
+
+var Mobil = mongoose.model('Mobil', mobilSchema);
+
+Mobil.findOne({ nama: "Aventador" },function (err, mobils) {
+	if (err)
+		return console.error(err);
+	console.log(mobils);
+})
+
+
---
+++ tambah-data/server.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mobil');
+
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('connection success');
+});
+
+var mobilSchema = mongoose.Schema({
+	nama: String,
+	merk: String,
+	tahunPembuatan: Number,
+	tanggalUpdate: Date,
+	transmisi: {
+		manual: Boolean,
+		outomatic: Boolean
+	}
+});
+
+var Mobil = mongoose.model('Mobil', mobilSchema);
+
+var lamborgini = new Mobil({
+	nama: 'Aventador',
+	merk: 'Lamborgini',
+	tahunPembuatan: 2015,
+	tanggalUpdate: new Date(),
+	transmisi: {
+		manual: true,
+		outomatic: false
+	}
+});
+
+var bmw = new Mobil({
+	nama: 'i8',
+	merk: 'BMW',
+	tahunPembuatan: 2015,
+	tanggalUpdate: new Date(),
+	transmisi: {
+		manual: false,
+		outomatic: true
+	}
+});
+
+bmw.save(function(err) {
+	if (err) {
+		console.log(err);
+	}
+	else {
+		console.log('berhasil menyimpan');
+	}
+});
+
+
